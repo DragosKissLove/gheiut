b961bde73415f3aff0903b9cf47343dc6c9d84a3
.regele <vi9lomister@gmail.com>
2025-05-15 19:52:13 +0100

    musicard.js

--- UI/banners/musicard.js
+++ UI/banners/musicard.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 const data = {
     backgroundImages: [
-        path.resolve(__dirname, '../musicimages/1.png'),
-        path.resolve(__dirname, '../musicimages/2.jpg'),
+        path.resolve(__dirname, '../musicimages/8.png'),
+        path.resolve(__dirname, '../musicimages/7.jpg'),
         path.resolve(__dirname, '../musicimages/3.jpg'),
         path.resolve(__dirname, '../musicimages/4.jpg'),
-        path.resolve(__dirname, '../musicimages/5.jpg'),
+        path.resolve(__dirname, '../musicimages/6.jpg'),
+        path.resolve(__dirname, '../musicimages/9.jpg'),
+        path.resolve(__dirname, '../musicimages/10.jpg'),
     ],
 };
 
