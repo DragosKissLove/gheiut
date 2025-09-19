fd3463f919f6313d43f3568a5a468e1ff279095d
.regele <vi9lomister@gmail.com>
2025-09-19 14:49:32 +0100

    Update lavalink.js

--- lavalink.js
+++ lavalink.js
@@ -2,9 +2,9 @@ module.exports = {
     enabled: true, 
     lavalink: {
         name: "GlaceYT",
-        password: "glace",
-        host: "5.39.63.207",
-        port:  8262,
-        secure: false
+        password: "youshallnotpass",
+        host: "lava.kasawa.pro",
+        port:  443,
+        secure: true
     }
 };
